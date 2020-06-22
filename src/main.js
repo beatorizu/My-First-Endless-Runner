062cf1b91a188c2844840d4feaaaa043b474a1e3
beatorizu <betrizaf@gmail.com>
2020-06-22 16:34:03 -0300

    feat: Play sound track

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 let scenarioImage;
 let character;
 let scenario;
+let soundTrack;
 
 function preload() {
   scenarioImage = loadImage("assets/images/scenario/forest.png");
   character = loadImage("assets/images/character/hipsta.png");
+  soundTrack = loadSound("assets/sounds/sound_track.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenario = new Scenario(scenarioImage, 3);
+  soundTrack.loop();
 }
 
 function draw() {
