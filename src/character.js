e81740d7cecd7d1bad77dcc1ad78b924f6625151
beatorizu <betrizaf@gmail.com>
2020-06-22 16:57:53 -0300

    feat: Hipsta walks

--- src/character.js
+++ src/character.js
@@ -1,9 +1,36 @@
 class Character {
   constructor(image) {
     this.image = image;
+    this.framesCoordinates = [
+      [0, 0],
+      [220, 0],
+      [440, 0],
+      [660, 0],
+      [0, 270],
+      [220, 270],
+      [440, 270],
+      [660, 270],
+      [0, 540],
+      [220, 540],
+      [440, 540],
+      [660, 540],
+      [0, 810],
+      [220, 810],
+      [440, 810],
+      [660, 810],
+    ];
+    this.currentFrame = 0;
   }
 
   show() {
-    image(this.image, 0, height - 135, 110, 135, 0, 0, 220, 270)
+    image(this.image, 0, height - 135, 110, 135, this.framesCoordinates[this.currentFrame][0], this.framesCoordinates[this.currentFrame][1], 220, 270);
+    this.walk();
+  }
+
+  walk() {
+    this.currentFrame++;
+    if (this.currentFrame >= this.framesCoordinates.length - 1) {
+      this.currentFrame = 0;
+    }
   }
 }
