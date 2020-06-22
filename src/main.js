b2f2f5569ecb662e74cd28ee35db02fa46913822
beatorizu <betrizaf@gmail.com>
2020-06-22 15:31:54 -0300

    feat: Load Hipsta

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 let scenario;
+let character;
 
 function preload() {
   scenario = loadImage("assets/images/scenario/forest.png");
+  character = loadImage("assets/images/character/hipsta.png");
 }
 
 function setup() {
@@ -10,4 +12,5 @@ function setup() {
 
 function draw() {
   background(scenario);
+  image(character, 0, height - 135, 110, 135, 0, 0, 220, 270);
 }
