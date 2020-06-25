8d922d574b69628e5f304f3f6b74ac49799c93f6
beatorizu <betrizaf@gmail.com>
2020-06-25 16:39:18 -0300

    feat: Add flying enemy

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const SCENARIO_IMAGE_PATH = "assets/images/scenario/forest.png";
 const HERO_IMAGE_PATH = "assets/images/character/hipsta.png";
 const ENEMY_GOTINHA_IMAGE_PATH = "assets/images/enemies/gotinha.png";
+const ENEMY_GOTINHA_VOADORA_IMAGE_PATH = "assets/images/enemies/gotinha-voadora.png";
 const SOUND_TRACK_PATH = "assets/sounds/sound_track.mp3";
 const GAME_OVER_SOUND_TRACK_PATH = "assets/sounds/game_over_sound_track.wav";
 const JUMP_SOUND_TRACK_PATH = "assets/sounds/jump_sound.mp3";
@@ -15,5 +16,12 @@ const ENEMY_GOTINHA_WIDTH_IN_SCREEN = 52;
 const ENEMY_GOTINHA_HEIGHT_IN_SCREEN = 52;
 const ENEMY_GOTINHA_SPRITE_WIDTH = 104;
 const ENEMY_GOTINHA_SPRITE_HEIGHT = 104;
+
+const ENEMY_GOTINHA_VOADORA_SPEED = 7;
+const ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN = 100;
+const ENEMY_GOTINHA_VOADORA_HEIGHT_IN_SCREEN = 75;
+const ENEMY_GOTINHA_VOADORA_SPRITE_WIDTH = 200;
+const ENEMY_GOTINHA_VOADORA_SPRITE_HEIGHT = 150;
+const ENEMY_GOTINHA_VOADORA_TOTAL_OF_SPRITES = 16;
 
 const HITBOX_PRECISION = .7;
--- src/main.js
+++ src/main.js
@@ -6,13 +6,17 @@ let gameOverSoundTrack;
 let hero;
 let enemyGotinha;
 let enemyGotinhaImage;
+let enemyGotinhaVoadora;
+let enemyGotinhaVoadoraImage;
 let jumpSoundTrack;
+const enemies = [];
 
 function preload() {
   scenarioImage = loadImage(SCENARIO_IMAGE_PATH);
   heroImage = loadImage(HERO_IMAGE_PATH);
   soundTrack = loadSound(SOUND_TRACK_PATH);
   enemyGotinhaImage = loadImage(ENEMY_GOTINHA_IMAGE_PATH);
+  enemyGotinhaVoadoraImage = loadImage(ENEMY_GOTINHA_VOADORA_IMAGE_PATH);
   gameOverSoundTrack = loadSound(GAME_OVER_SOUND_TRACK_PATH);
   jumpSoundTrack = loadSound(JUMP_SOUND_TRACK_PATH);
   jumpSoundTrack.setVolume(.4);
@@ -23,6 +27,8 @@ function setup() {
   scenario = new Scenario(scenarioImage, 3);
   hero = new Hero(heroImage, 0, HERO_WIDTH_IN_SCREEN, HERO_HEIGHT_IN_SCREEN, HERO_SPRITE_WIDTH, HERO_SPRITE_HEIGHT);
   enemyGotinha = new Enemy(enemyGotinhaImage, width - ENEMY_GOTINHA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_SPRITE_WIDTH, ENEMY_GOTINHA_SPRITE_HEIGHT, ENEMY_GOTINHA_SPEED);
+  enemyGotinhaVoadora = new Enemy(enemyGotinhaVoadoraImage, width - ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_VOADORA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_VOADORA_SPRITE_WIDTH, ENEMY_GOTINHA_VOADORA_SPRITE_HEIGHT, ENEMY_GOTINHA_VOADORA_SPEED, ENEMY_GOTINHA_VOADORA_TOTAL_OF_SPRITES);
+  enemies.push(enemyGotinha, enemyGotinhaVoadora);
   soundTrack.loop();
   frameRate(40);
 }
@@ -50,10 +56,11 @@ function draw() {
   scenario.move();
   hero.show();
   hero.pullByTheGravity();
-  enemyGotinha.show();
-  enemyGotinha.move();
-
-  if (hero.isColliding(enemyGotinha)) {
-    gameOver();
-  }
+  enemies.forEach(enemy => {
+    enemy.show();
+    enemy.move();
+    if (hero.isColliding(enemy)) {
+      gameOver();
+    }
+  });
 }
