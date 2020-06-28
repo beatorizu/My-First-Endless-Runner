8a674d9d1b5b8cbf603d8f9a3514ffbbcffbd336
beatorizu <betrizaf@gmail.com>
2020-06-28 19:28:49 -0300

    fix: Keep the image ratio
    Calculate the image width keeping the ratio to not distort the scenario.

--- src/scenario.js
+++ src/scenario.js
@@ -1,24 +1,26 @@
 class Scenario {
-  constructor(image, speed) {
-    this.image = image;
-    this.speed = speed;
-    this.x1 = 0;
-    this.x2 = width;
-  }
+	constructor(image, speed) {
+		this.image = image;
+		this.speed = speed;
+		this.ratio = height / this.image.height;
+		this.newWidth = parseInt(this.ratio * this.image.width);
+		this.x1 = 0;
+		this.x2 = this.newWidth;
+	}
 
-  show() {
-    image(this.image, this.x1, 0, width, height);
-    image(this.image, this.x2, 0, width, height);
-  }
+	show() {
+		image(this.image, this.x1, 0, this.newWidth, height);
+		image(this.image, this.x2, 0, this.newWidth, height);
+	}
 
-  move() {
-    this.x1 -= this.speed;
-    this.x2 -= this.speed;
-    if (this.x1 < -width) {
-      this.x1 = width;
-    }
-    if (this.x2 < -width) {
-      this.x2 = width;
-    }
-  }
+	move() {
+		this.x1 -= this.speed;
+		this.x2 -= this.speed;
+		if (this.x1 < -this.newWidth) {
+			this.x1 = this.newWidth;
+		}
+		if (this.x2 < -this.newWidth) {
+      this.x2 = this.newWidth;
+		}
+	}
 }
