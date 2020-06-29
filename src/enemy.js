d0813c1e088eac673bb7492d014fa3958fef2fb9
beatorizu <betrizaf@gmail.com>
2020-06-29 16:58:29 -0300

    feat: Empower the next enemy

--- src/enemy.js
+++ src/enemy.js
@@ -1,13 +1,18 @@
 class Enemy extends Character {
-  constructor(image, x, yLevel, widthInScreen, heightInScreen, widthSprite, heightSprite, speed, totalOfSprites) {
+  constructor(image, x, yLevel, hasPower, widthInScreen, heightInScreen, widthSprite, heightSprite, speed, totalOfSprites) {
     super(image, x, yLevel, widthInScreen, heightInScreen, widthSprite, heightSprite, totalOfSprites)
     this.speed = speed;
+    this.hasPower = hasPower;
   }
 
-  move() {
-    this.x -= this.speed;
-    if (this.x <= -this.width) {
-      this.x = width
+  move(enemy) {
+    if (this.hasPower) {
+      this.x -= this.speed;
+      if (this.x <= -this.width) {
+        this.x = width
+        this.hasPower = false
+        enemy.hasPower = true
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -36,9 +36,9 @@ function setup() {
     element.scenario = new Scenario(image, speed);
   });
   hero = new Hero(heroImage, 0, Y_GROUND_LEVEL, HERO_WIDTH_IN_SCREEN, HERO_HEIGHT_IN_SCREEN, HERO_SPRITE_WIDTH, HERO_SPRITE_HEIGHT);
-  enemyGotinha = new Enemy(enemyGotinhaImage, width - ENEMY_GOTINHA_WIDTH_IN_SCREEN, Y_GROUND_LEVEL, ENEMY_GOTINHA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_SPRITE_WIDTH, ENEMY_GOTINHA_SPRITE_HEIGHT, ENEMY_GOTINHA_SPEED);
-  enemyGotinhaVoadora = new Enemy(enemyGotinhaVoadoraImage, width - ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN, Y_AIR_LEVEL, ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_VOADORA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_VOADORA_SPRITE_WIDTH, ENEMY_GOTINHA_VOADORA_SPRITE_HEIGHT, ENEMY_GOTINHA_VOADORA_SPEED, ENEMY_GOTINHA_VOADORA_TOTAL_OF_SPRITES);
-  enemyTroll = new Enemy(enemyTrollImage, width - ENEMY_TROLL_WIDTH_IN_SCREEN, 0, ENEMY_TROLL_WIDTH_IN_SCREEN, ENEMY_TROLL_HEIGHT_IN_SCREEN, ENEMY_TROLL_SPRITE_WIDTH, ENEMY_TROLL_SPRITE_HEIGHT, ENEMY_TROLL_SPEED, ENEMY_TROLL_TOTAL_OF_SPRITES);
+  enemyGotinha = new Enemy(enemyGotinhaImage, width, Y_GROUND_LEVEL, true, ENEMY_GOTINHA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_SPRITE_WIDTH, ENEMY_GOTINHA_SPRITE_HEIGHT, ENEMY_GOTINHA_SPEED, 0);
+  enemyGotinhaVoadora = new Enemy(enemyGotinhaVoadoraImage, width, Y_AIR_LEVEL, false, ENEMY_GOTINHA_VOADORA_WIDTH_IN_SCREEN, ENEMY_GOTINHA_VOADORA_HEIGHT_IN_SCREEN, ENEMY_GOTINHA_VOADORA_SPRITE_WIDTH, ENEMY_GOTINHA_VOADORA_SPRITE_HEIGHT, ENEMY_GOTINHA_VOADORA_SPEED, ENEMY_GOTINHA_VOADORA_TOTAL_OF_SPRITES);
+  enemyTroll = new Enemy(enemyTrollImage, width, 0, false, ENEMY_TROLL_WIDTH_IN_SCREEN, ENEMY_TROLL_HEIGHT_IN_SCREEN, ENEMY_TROLL_SPRITE_WIDTH, ENEMY_TROLL_SPRITE_HEIGHT, ENEMY_TROLL_SPEED, ENEMY_TROLL_TOTAL_OF_SPRITES);
   enemies.push(enemyGotinha, enemyGotinhaVoadora, enemyTroll);
   soundTrack.loop();
   frameRate(40);
@@ -74,9 +74,9 @@ function draw() {
   score.addPoint();
   hero.show();
   hero.pullByTheGravity();
-  enemies.forEach(enemy => {
+  enemies.forEach((enemy, index, array) => {
     enemy.show();
-    enemy.move();
+    enemy.move(array[(index + 1) % array.length]);
     if (hero.isColliding(enemy)) {
       gameOver();
     }
